aa8516f06a9be8b0cbf067e4583435bf8c28318c
bouaik <bouaik.lhou@outlook.fr>
2020-04-27 09:00:27 +0000

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :username, presence: true, length: {maximum: 50, minimum: 3}, uniqueness: {case_sensitive: false}
+    validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+end
