cae9fc4e967b830fbe02c5f18a2d34f403509e07
bouaik <bouaik.lhou@outlook.fr>
2020-04-27 09:26:57 +0000

    fix rubocop offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  validates :title, presence: true, length: {maximum: 300, minimum: 5}
-  validates :body, presence: true, length: { minimum: 5}
+  validates :title, presence: true, length: { maximum: 300, minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
