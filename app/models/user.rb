49524c2d78b5518ffba08a7bead495df8dc4b139
bouaik <bouaik.lhou@outlook.fr>
2020-04-27 09:21:00 +0000

    create comment model with it's validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_many :post
+    has_many :posts
+    has_many :comments
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :username, presence: true, length: {maximum: 50, minimum: 3}, uniqueness: {case_sensitive: false}
     validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
